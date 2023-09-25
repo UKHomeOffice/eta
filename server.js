6bf6154ed60b9f98d0f8fc75457d011e5536d80b
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2023-09-25 10:14:14 +0100

    removed BETA banner

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ if (process.env.REDIS_URL) {
 const app = hof(settings);
 app.use((req, res, next) => {
   res.locals.htmlLang = 'en';
-  res.locals.feedbackUrl = 'https://eforms.homeoffice.gov.uk/outreach/feedback.ofml';
   next();
 });
 
