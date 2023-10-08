0ab8476a9d12c3ea2451c4f45ad7da485feec035
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2023-10-08 22:29:02 +0100

    add redis config

--- config.js
+++ config.js
@@ -15,5 +15,9 @@ module.exports = {
   },
   hosts: {
     acceptanceTests: process.env.ACCEPTANCE_HOST_NAME || `http://localhost:${process.env.PORT || 8080}`
-  }
+  },
+  redis: {
+    port: process.env.REDIS_PORT || '6379',
+    host: process.env.REDIS_HOST || '127.0.0.1'
+  },
 };
