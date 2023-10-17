7efbdc604634a6e80bfded16047177b4133010e9
Muhammed Islam <muhammed.islam@digital.homeoffice.gov.uk>
2023-10-17 18:43:02 +0100

    Fixed issue with question field special character validation

--- apps/eta/fields/index.js
+++ apps/eta/fields/index.js
@@ -64,7 +64,7 @@ module.exports = {
     isPageHeading: false,
     labelClassName: 'visuallyhidden',
     mixin: 'textarea',
-    validate: ['required', 'notUrl', { type: 'regex', arguments: /^[\s\S]{1,2000}$/ },
+    validate: ['required', 'notUrl', { type: 'regex', arguments: /^[^\[\]\|<>]{1,2000}$/ },
       { type: 'maxlength', arguments: 2000 }],
     attributes: [{
       attribute: 'rows',
@@ -75,7 +75,7 @@ module.exports = {
     isPageHeading: false,
     labelClassName: 'visuallyhidden',
     mixin: 'textarea',
-    validate: ['required', 'notUrl', { type: 'regex', arguments: /^[\s\S]{1,2000}$/ },
+    validate: ['required', 'notUrl', { type: 'regex', arguments: /^[^\[\]\|<>]{1,2000}$/ },
       { type: 'maxlength', arguments: 2000 }],
     attributes: [{
       attribute: 'rows',
